b2a7dcc0292c350bd85791b5ce8a041a67210d50
Ryan Halon <rhalon7@gmail.com>
2025-10-09 19:46:50 -0400

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className="text-2xl underline">Welcome to my SAAS APp</h1>
+      <Button>Let's Get Started</Button>
+    </div>
+  )
+}
+
+export default Page
