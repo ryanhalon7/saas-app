4b821e88368b7b48ea13472268aa8b1acb80ec8c
Ryan Halon <rhalon7@gmail.com>
2025-10-14 14:03:39 -0400

    Implement companion builder form

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -19,7 +21,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${bricolage.variable} antialiased`}>{children}</body>
-    </html>
+        <body className={`${bricolage.variable} antialiased`}>
+          <ClerkProvider appearance={{ variables: {colorPrimary: '#fe5933' }} }>
+            <Navbar />
+            {children}
+          </ClerkProvider>
+        </body>
+      </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,53 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my SAAS APp</h1>
-      <Button>Let's Get Started</Button>
+    <div className="pt-12 flex flex-col items-center">
+      <div>
+      <h1 className="text-3xl mb-4 pl-3">Dashboard</h1>
+
+        <section className='home-section'>
+          <CompanionCard
+            id="123"
+            name="Neura the Brainy Explorer"
+            topic="Neural Network of the Brain"
+            subject="Science"
+            duration={45}
+            color="#ffda6e"
+          />
+          <CompanionCard
+            id="456"
+            name="Countsy the Number Wizard"
+            topic="Derivatives & Integrals"
+            subject="Maths"
+            duration={30}
+            color="#e5d0ff"
+          />
+          <CompanionCard
+            id="789"
+            name="Verba the Vocabulary Builder"
+            topic="English Literature"
+            subject="Language"
+            duration={30}
+            color="#bde7ff"
+          />
+        </section>
+
+
+        <section className='home-section'>
+          <CompanionsList 
+            title="Recently completed sessions"
+            companions={recentSessions}
+            classNames="w-2/3 max-lg:w-full"
+          />
+          <CTA/>
+        </section>
+      </div>
     </div>
   )
 }
