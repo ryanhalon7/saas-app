f6ad28e77963bf661adf0c0e523865c61a875972
Ryan Halon <rhalon7@gmail.com>
2025-10-24 22:49:24 -0400

    Implement subscription restrictions and enforcements

--- app/page.tsx
+++ app/page.tsx
@@ -3,49 +3,37 @@ import CompanionsList from '@/components/CompanionsList'
 import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
 import { recentSessions } from '@/constants'
+import { getAllCompanions, getRecentSessions } from '@/lib/actions/companion.actions'
+import { getSubjectColor } from '@/lib/utils'
 import React from 'react'
 
-const Page = () => {
+const Page = async () => {
+  const companions = await getAllCompanions({ limit: 3 });
+  const recentSessionsCompanions = await getRecentSessions(10);
+
   return (
     <div className="pt-12 flex flex-col items-center">
       <div>
       <h1 className="text-3xl mb-4 pl-3">Dashboard</h1>
 
         <section className='home-section'>
-          <CompanionCard
-            id="123"
-            name="Neura the Brainy Explorer"
-            topic="Neural Network of the Brain"
-            subject="Science"
-            duration={45}
-            color="#ffda6e"
+          {companions.map((companion) => (
+            <CompanionCard
+              key={companion.id}
+              {... companion}
+              color={getSubjectColor(companion.subject)}
           />
-          <CompanionCard
-            id="456"
-            name="Countsy the Number Wizard"
-            topic="Derivatives & Integrals"
-            subject="Maths"
-            duration={30}
-            color="#e5d0ff"
-          />
-          <CompanionCard
-            id="789"
-            name="Verba the Vocabulary Builder"
-            topic="English Literature"
-            subject="Language"
-            duration={30}
-            color="#bde7ff"
-          />
+          ))}
         </section>
 
 
         <section className='home-section'>
           <CompanionsList 
             title="Recently completed sessions"
-            companions={recentSessions}
+            companions={recentSessionsCompanions}
             classNames="w-2/3 max-lg:w-full"
           />
-          <CTA/>
+          <CTA />
         </section>
       </div>
     </div>
